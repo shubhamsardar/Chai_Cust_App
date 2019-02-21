8dbb9b21a28176d61e438298ffc65d85324a5f52
name <prathamesh.kondaskar@gmail.com>
2019-02-21 11:35:40 +0530

    new screen

---
+++ app/src/main/java/in/co/tripin/chahiyecustomer/Activities/FavouriteTapri.java
@@ -0,0 +1,15 @@
+package in.co.tripin.chahiyecustomer.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.co.tripin.chahiyecustomer.R;
+
+public class FavouriteTapri extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite_tapri);
+    }
+}
