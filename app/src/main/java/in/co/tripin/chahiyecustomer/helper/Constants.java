6fedc5fa05bc00da6281776eb8eccb233a1b9280
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-03-15 13:20:10 +0530

    UI Fixes

--- app/src/main/java/in/co/tripin/chahiyecustomer/helper/Constants.java
+++ app/src/main/java/in/co/tripin/chahiyecustomer/helper/Constants.java
@@ -2,8 +2,8 @@ package in.co.tripin.chahiyecustomer.helper;
 
 public final class Constants {
 
-//    public static final String BASE_URL = "http://206.189.135.19:3055/";
-    public static final String BASE_URL = "http://e63ae891.ngrok.io/";
+    public static final String BASE_URL = "http://206.189.135.19:3055/";
+//    public static final String BASE_URL = "http://e63ae891.ngrok.io/";
     public static final String ADMIN_PHONE = "8394876737";
 
     public static final String M_ID = "VRIVFR23016900670373";
