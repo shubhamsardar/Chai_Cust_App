b9659bfff3d72133d25df6b6550b88dffd9b03b9
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-04-03 16:12:33 +0530

    New attempt

---
+++ app/src/main/java/in/co/tripin/chahiyecustomer/helper/MyAppGlideModule.java
@@ -0,0 +1,26 @@
+package in.co.tripin.chahiyecustomer.helper;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        super.applyOptions(context, builder);
+        builder.setDefaultRequestOptions(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL));
+    }
+
+    @Override
+    public boolean isManifestParsingEnabled() {
+        return false;
+    }
+
+}
